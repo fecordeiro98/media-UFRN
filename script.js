9cfab5252c3fe5d09f6e4cd132d26bea589095e4
FCordeiro <162184965+fecordeiro98@users.noreply.github.com>
2025-07-20 20:08:18 -0300

    grande atualização

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+const resultado = document.getElementById('resultado');
+
 function calcular() {
     let unidade1 = parseFloat(document.getElementById('u1').value);
     let unidade2 = parseFloat(document.getElementById('u2').value);
     let unidade3 = parseFloat(document.getElementById('u3').value);
-    let media = (unidade1 + unidade2 + unidade3) / 3;
-    document.getElementById('media').value = media.toFixed(1);
+    let media = ((unidade1 + unidade2 + unidade3) / 3).toFixed(1);
+    document.getElementById('media').value = media;
+
+    if (media < 6) {
+        resultado.textContent = 'Estudante reprovado por média!';
+        resultado.style.color = 'red';
+        resultado.style.fontWeight = 'bold';
+    } else if (unidade1 < 4 || unidade2 < 4 || unidade3 < 4) {
+        resultado.textContent = 'Estudante em recuperação (uma nota abaixo de 4)!';
+        resultado.style.color = 'orange';
+        resultado.style.fontWeight = 'bold';
+    } else {
+        resultado.textContent = 'Estudante aprovado!';
+        resultado.style.color = '#164194';
+        resultado.style.fontWeight = 'bold';
+    }
 }
